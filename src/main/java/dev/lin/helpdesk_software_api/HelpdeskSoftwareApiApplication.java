608b8579062303759d5d1729eba3ec52c8fe8894
Lin <lincarbajales@gmail.com>
2025-08-25 15:12:46 +0200

    init: comit init

---
+++ src/main/java/dev/lin/helpdesk_software_api/HelpdeskSoftwareApiApplication.java
@@ -0,0 +1,13 @@
+package dev.lin.helpdesk_software_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskSoftwareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskSoftwareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lin/helpdesk_software_api/HelpdeskSoftwareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lin.helpdesk_software_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskSoftwareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
